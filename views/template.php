2562f863028bba675bd291492783fda71e3a847a
Salumao Barbosa da Costa <salubcosta@gmail.com>
2016-12-30 08:26:26 -0300

    modelo html do projeto

---
+++ views/template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="uft-8">
+	<title>Projeto Hora-Extra</title>
+</head>
+<body>
+
+<?php
+	$this->carregarView($view, $dados);
+?>
+</body>
+</html>
