6a0d1b4597b595a6cd941b8862f6a31cc6ac728d
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-01-03 12:06:42 -0300

    criação do views login.php

---
+++ views/login.php
@@ -0,0 +1,13 @@
+<div class="formulario">
+	<form method="POST">
+		<p>
+			<input type="text" name="usuario" placeholder="Informe usuário" required="true" class="input">
+		</p>
+		<p>
+			<input type="password" name="senha" placeholder="Informe senha" required="true" class="input">
+		</p>
+		<p>
+			<button>Acessar</button>
+		</p>
+	</form>		
+</div>
