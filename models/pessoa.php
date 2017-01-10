141b7d3ef380adc00c5938f36b8ce9adb60e55e5
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-01-10 08:09:43 -0300

    mudança de autenticação com o banco de dados para usuários no AD - utilizando funcão ldap

--- models/pessoa.php
+++ models/pessoa.php
@@ -6,21 +6,54 @@ class Pessoa extends model{
 
 		$dados = array();
 
-		$sql = 'SELECT id, nome, usuario, senha from pessoa where usuario = :usuario and senha = :senha';
-		$sql = $this->db->prepare($sql);
-		$sql->bindValue(':usuario',addslashes($usuario)); //ADDSLASHES adiciona uma barra invertida antes de cada aspa simples e dupla
-		$sql->bindValue(':senha', md5(addslashes($senha)));
+		if($this->autenticarNoDomino($usuario, $senha)){
+			$dados['usuario'] = $usuario;
+			$dados['senha'] = md5($senha);
 
-		$sql->execute();
-
-		if($sql->rowCount() > 0 ){
-			$dados =  $sql->fetch();
-
-			$_SESSION['id'] = $dados['id'];
-			$_SESSION['nome'] = $dados['nome'];
-			$_SESSION['usuario'] = $dados['nome'];
+			$_SESSION['usuario'] = $dados['usuario'];
+			$_SESSION['senha'] = $dados['senha'];
 		}
+		/*
+			$sql = 'SELECT id, nome, usuario, senha from pessoa where usuario = :usuario and senha = :senha';
+			$sql = $this->db->prepare($sql);
+			$sql->bindValue(':usuario',addslashes($usuario)); //ADDSLASHES adiciona uma barra invertida antes de cada aspa simples e dupla
+			$sql->bindValue(':senha', md5(addslashes($senha)));
+
+			$sql->execute();
+
+			if($sql->rowCount() > 0 ){
+				$dados =  $sql->fetch();
+
+				$_SESSION['id'] = $dados['id'];
+				$_SESSION['nome'] = $dados['nome'];
+				$_SESSION['usuario'] = $dados['nome'];
+			}
+		*/
 
 		return $dados;
+	}
+
+	public function autenticarNoDomino($user, $pass){
+		ini_set('error_reporting',E_ALL);
+		ini_set('display_errors',1);
+
+		$ldapconfig['host']="10.98.0.5";
+		$ldapconfig['port']="389";
+		$domain="EBSERHNET";
+
+		$grupo='ou=hdt,ou=ebserh,dc=ebserhnet,dc=ebserh,dc=gov,dc=br';
+		$filtro="(&(objectCategory=user)(memberOf=DN DO GRUPO))";
+
+		$ds=ldap_connect($ldapconfig['host'], $ldapconfig['port']);
+		ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+		ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+
+		$bind=ldap_bind($ds, $user .'@'.$domain, $pass);
+
+		if ($bind) {
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
