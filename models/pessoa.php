0ca4223cdb0f69b76d72d2dc70371391a9ac8fba
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-01-18 15:18:56 -0300

    atualização na forma como faz a autenticação do usuário e senha

--- models/pessoa.php
+++ models/pessoa.php
@@ -13,23 +13,7 @@ class Pessoa extends model{
 			$_SESSION['usuario'] = $dados['usuario'];
 			$_SESSION['senha'] = $dados['senha'];
 		}
-		/*
-			$sql = 'SELECT id, nome, usuario, senha from pessoa where usuario = :usuario and senha = :senha';
-			$sql = $this->db->prepare($sql);
-			$sql->bindValue(':usuario',addslashes($usuario)); //ADDSLASHES adiciona uma barra invertida antes de cada aspa simples e dupla
-			$sql->bindValue(':senha', md5(addslashes($senha)));
-
-			$sql->execute();
-
-			if($sql->rowCount() > 0 ){
-				$dados =  $sql->fetch();
-
-				$_SESSION['id'] = $dados['id'];
-				$_SESSION['nome'] = $dados['nome'];
-				$_SESSION['usuario'] = $dados['nome'];
-			}
-		*/
-
+		
 		return $dados;
 	}
 
